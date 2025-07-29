a8bb7b8e63dab7de25e3901c1b52f63192c287c1
Looksini David <looksinidavid06@gmail.com>
2025-07-29 21:34:08 +0530

    initial version v1.0

---
+++ Student Grade Tracker/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Student_Grade_Tracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtStudent = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAddGrade = new System.Windows.Forms.Button();
+            this.lstGrades = new System.Windows.Forms.ListBox();
+            this.txtGrade = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtStudent
+            // 
+            this.txtStudent.Location = new System.Drawing.Point(150, 63);
+            this.txtStudent.Name = "txtStudent";
+            this.txtStudent.Size = new System.Drawing.Size(179, 22);
+            this.txtStudent.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Student";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Grade";
+            // 
+            // btnAddGrade
+            // 
+            this.btnAddGrade.Location = new System.Drawing.Point(225, 182);
+            this.btnAddGrade.Name = "btnAddGrade";
+            this.btnAddGrade.Size = new System.Drawing.Size(104, 35);
+            this.btnAddGrade.TabIndex = 4;
+            this.btnAddGrade.Text = "ADD";
+            this.btnAddGrade.UseVisualStyleBackColor = true;
+            this.btnAddGrade.Click += new System.EventHandler(this.BtnAddGrade_Click);
+            // 
+            // lstGrades
+            // 
+            this.lstGrades.FormattingEnabled = true;
+            this.lstGrades.ItemHeight = 16;
+            this.lstGrades.Location = new System.Drawing.Point(64, 240);
+            this.lstGrades.Name = "lstGrades";
+            this.lstGrades.Size = new System.Drawing.Size(429, 180);
+            this.lstGrades.TabIndex = 5;
+            // 
+            // txtGrade
+            // 
+            this.txtGrade.Location = new System.Drawing.Point(150, 123);
+            this.txtGrade.Name = "txtGrade";
+            this.txtGrade.Size = new System.Drawing.Size(179, 22);
+            this.txtGrade.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(678, 455);
+            this.Controls.Add(this.txtGrade);
+            this.Controls.Add(this.lstGrades);
+            this.Controls.Add(this.btnAddGrade);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtStudent);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtStudent;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAddGrade;
+        private System.Windows.Forms.ListBox lstGrades;
+        private System.Windows.Forms.TextBox txtGrade;
+    }
+}
+
